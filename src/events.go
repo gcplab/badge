1d1b1b0d95b7e3f1640a1f013365efde692ebdd9
SeongJae Park <sj38.park@gmail.com>
2013-07-24 21:53:00 +0900

    First commit for events page
    Just for template work
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ src/events.go
@@ -0,0 +1,27 @@
+// Copyright 2013 GCP Lab. All rights reserved.
+// Use of this sorce code is governed by the MIT license
+// that can be found in the LICENSE file.
+
+package badge
+
+import (
+    "net/http"
+    "text/template"
+)
+
+var (
+    eventsTmpl *template.Template =
+            template.Must(template.ParseFiles("templates/events.html"))
+)
+
+func init() {
+    http.HandleFunc("/events", eventsHandler)
+}
+
+func eventsHandler(w http.ResponseWriter, r *http.Request) {
+    // Do nothing now...
+    err := eventsTmpl.Execute(w, nil)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}         
