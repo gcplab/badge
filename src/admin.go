95f90d01ddf9edbbf8358fffba682ab6a945b952
Jongmin Kim <atomaths@gmail.com>
2013-07-31 19:34:25 +0900

    fix fmt (dev branch test)

--- src/admin.go
+++ src/admin.go
@@ -5,21 +5,21 @@
 package badge
 
 import (
-  "net/http"
-  "html/template"
+	"html/template"
+	"net/http"
 )
 
 func init() {
-  http.HandleFunc("/admin", admin)
+	http.HandleFunc("/admin", admin)
 }
 
 type Admin struct {
-  Name string
-  Grant string
+	Name  string
+	Grant string
 }
 
 func admin(w http.ResponseWriter, r *http.Request) {
-  admin := Admin{Name: "admin", Grant: "gran2t"}
-  temp, _ := template.ParseFiles("templates/admin.html")
-  temp.Execute(w, admin)
+	admin := Admin{Name: "admin", Grant: "gran2t"}
+	temp, _ := template.ParseFiles("templates/admin.html")
+	temp.Execute(w, admin)
 }
